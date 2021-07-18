d7db6f39a641afccce790256a07849cfce1d03b1
swatantra <swatantra440ry@gmail.com>
2021-07-18 15:49:29 +0545

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+   <q-card
+      class="my-card text-white row"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, perspiciatis.
+      </q-card-section>
+    </q-card>
+</template>
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
