5a9caf6719565e2c891ad1bd2e29e09e905aca92
swatantra <swatantra440ry@gmail.com>
2021-07-19 12:54:14 +0545

    render data from api

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,54 @@
 <template>
-   <q-card
-      class="my-card text-white row"
-      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-    >
-      <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, perspiciatis.
-      </q-card-section>
-    </q-card>
+<q-page padding>
+<div class="q-pb-md">
+<q-input class="q-pt-md" outlined v-model="title" label="Title" />
+<q-input class="q-pt-md q-pb-md" outlined v-model="content" label="Content" />
+<q-btn color="primary" label="Submit" />
+<q-spinner class="q-pt-md"
+      v-if="loading"
+      color="primary"
+      size="3em"
+    />
+</div>
+<h4>Todo List</h4>
+<div class="q-pa-md scroll" style="height: 300px">
+      <div  v-for="(post, index) in posts" :key="index" class="q-mb-sm">
+        <q-badge color="secondary">
+           {{ posts.length - index }}
+        </q-badge>
+        {{ post.data.title }}
+      </div>
+  </div>
+  </q-page>
 </template>
 <script>
-import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'PageIndex'
-})
+import axios from 'axios'
+export default {
+  name: 'PageIndex',
+  created () {
+    axios.get('https://www.reddit.com/r/aww.json?raw_json=1')
+      .then(response => {
+        this.loading = false
+        this.posts = response.data.data.children
+      })
+      .catch(error => {
+        this.loading = false
+        console.log(error)
+      })
+  },
+  data () {
+    return {
+      showImageDialog: false,
+      imageUrl: '',
+      loading: true,
+      posts: []
+    }
+  },
+  methods: {
+    showImage (image) {
+      this.showImageDialog = true
+      this.imageUrl = image
+    }
+  }
+}
 </script>
